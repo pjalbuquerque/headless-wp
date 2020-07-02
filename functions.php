9e390d4bbea6ecc146ceb87ef225e999a5938570
Paulo Albuquerque <paulo.albuquerque@pixter.com.br>
2020-07-02 07:11:50 -0300

    Theme WP Headless

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Theme for the Headless WordPress.
+ *
+ *
+ * @package  Headless_WP
+ */
+
+// Frontend origin.
+require_once 'inc/frontend-origin.php';
+
+// ACF commands.
+require_once 'inc/class-acf-commands.php';
+
+// Logging functions.
+require_once 'inc/log.php';
+
+// CORS handling.
+require_once 'inc/cors.php';
+
+// Admin modifications.
+require_once 'inc/admin.php';
+
+// Add Menus.
+require_once 'inc/menus.php';
+
+// Add Headless Settings area.
+require_once 'inc/acf-options.php';
+
+// Add GraphQL resolvers.
+require_once 'inc/graphql/resolvers.php';
---
+++ inc/frontend-origin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Frontend origin helper function.
+ *
+ * @package  Headless_WP
+ */
+
+/**
+ * Placeholder function for determining the frontend origin.
+ *
+ * @TODO Determine the headless client's URL based on the current environment.
+ *
+ * @return str Frontend origin URL, i.e., http://localhost:3000.
+ */
+function get_frontend_origin() {
+
+    if ( !FRONTEND_ORIGIN ) {
+        define('FRONTEND_ORIGIN', 'http://localhost:3000');
+    }
+    return FRONTEND_ORIGIN;
+}
